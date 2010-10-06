037725741ab4d6f1ea82ef591f9ea3782bbe98f2
Lance Quejada <voidnothings@live.com>
2010-10-06 22:50:43 +0800

    added autotest and exercices for chapter 3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,23 @@
 class PagesController < ApplicationController
+
   def home
     @title = "Home"
+    @base_title ="Ruby on Rails Tutorial Sample App"
   end
 
   def contact
     @title = "Contact"
+    @base_title ="Ruby on Rails Tutorial Sample App"
   end
   
   def about
     @title = "About"
+    @base_title ="Ruby on Rails Tutorial Sample App"
+  end
+  
+  def help
+    @title = "Help"
+    @base_title ="Ruby on Rails Tutorial Sample App"
   end
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+  
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -41,4 +45,17 @@ describe PagesController do
                         :content => "Ruby on Rails Tutorial Sample App | About")            
     end    
   end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", 
+                        :content => "Ruby on Rails Tutorial Sample App | Help")            
+    end    
+  end  
 end
